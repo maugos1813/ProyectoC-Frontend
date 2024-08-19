db4d84041daf206e5a694f25bbcf79fc1d2c0dcf
Ronny <ronnyd.1994@gmail.com>
2024-08-19 18:10:45 -0500

    examenes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Examns } from "./components/Examns";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="text-red-500">hola a todos</div>
+     <Examns/>
     </>
   );
 }
