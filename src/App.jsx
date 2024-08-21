00a82c314ab5892c141c542015a94bdd731e3f43
Ronny <ronnyd.1994@gmail.com>
2024-08-20 22:45:43 -0500

    faltan funcionalidades

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Examns } from './components/Examns'
+import { Exam } from './components/Exam'
+import { RecordView } from './components/RecordVideo'
 
 export default function App() {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/' element={ <Examns/>} />
+      <Route path='/:id' element={ <Exam/>} />
+      <Route path='/video' element={ <RecordView/>} />
+    </Routes>
+   
   )
 }
--- src/components/Examns.jsx
+++ src/components/Examns.jsx
@@ -1,27 +1,29 @@
 import React from 'react'
-import Data from './../data.json'
+import Data from './../examenCompleto.json'
+import { Link } from 'react-router-dom'
 
 export const Examns = () => {
-    console.log(Data);
+
     return (
-        <main className='h-[85%] w-full py-8 '>
+        <main className=' w-full h-[90%] py-8 bg-[#baf1cf] '>
             <h1 className='font-semibold text-[50px] text-center pb-4'>Examenes</h1>
             <section className='grid - grid-cols-2 py-4 px-8 gap-8 w-full'>
                 {Data &&
-                    Data.map((dato,index) => 
+                    Data.map((dato, index) =>
 
-                        (
-                            <section key={index} className='flex flex-row bg-[#F4F4F5] shadow-xl items-center justify-around rounded-2xl p-4 gap-4'>
-                                <div className='flex flex-col'>
-                                    <h2 className='font-semibold text-[35px]' > {dato.titulo}</h2>
-                                    <span className=''>{dato.fecha}</span>
-                                </div>
-                                <button className='bg-[#0A8537] text-white font-semibold w-[25%] h-[40%] rounded-3xl justify-center flex items-center hover:bg-[#1f6e3c]'>Access</button>
-                            </section>
-                        )
+                    (
+                        <section key={index} className='flex flex-row bg-[#F4F4F5] shadow-xl items-center justify-between rounded-2xl p-4 gap-4'>
+                            <div className='flex flex-col'>
+                                <h2 className='font-semibold text-[35px]' > {dato.name}</h2>
+                                <span className=''>{dato.autor}</span>
+                            </div>
+                            <div className="w-full h-full flex justify-end items-center ">
+                                <Link to={`/${dato.id}`} className='bg-[#0A8537] text-white font-semibold w-[8rem] h-[30%] rounded-3xl justify-center flex items-center hover:bg-[#1f6e3c]'>Access</Link> </div>
+                        </section>
+                    )
                     )
                 }
-                
+
             </section>
         </main>
     )
