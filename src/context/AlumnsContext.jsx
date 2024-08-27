ed162bed1f7bc696810f5ece49acbb31f5b5f79d
Ronny <ronnyd.1994@gmail.com>
2024-08-27 14:21:21 -0500

    sav

--- src/context/AlumnsContext.jsx
+++ src/context/AlumnsContext.jsx
@@ -35,7 +35,7 @@ export const AuthProvider = ({ children }) => {
     return (
         <AlumnsContext.Provider
             value={{
-                alumns,
+                
                 user,
                 isLoading,
                 isError,
