2c98f40d7a980403bc1af5779fc1fa21b2f70e25
Mauro Agostinelli <maufabagosgam@gmail.com>
2024-09-02 09:40:44 -0500

    estilos examas

--- src/pagesM/EditExams.jsx
+++ src/pagesM/EditExams.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import fondo from "../public/lice.jpeg";
 
 export const EditExams = () => {
   return (
-    <div>EditExams</div>
-  )
-}
+    <div
+      className=" flex flex-col gap-2 h-screen bg-cover bg-center mt-2"
+      style={{ backgroundImage: `url(${fondo})` }}
+    >
+      <div className="flex gap-2 mt-3">
+        <div className="flex-1 border border-sky-400 flex justify-center h-[40vh] rounded-3xl items-center bg-white bg-opacity-70">
+          <button className="border-sky-400 border h-[8vh] w-[20vh]">Elementary</button>
+        </div>
+        <div className="flex-1 border border-sky-400 flex justify-center rounded-3xl items-center bg-white bg-opacity-70">
+          <button className="border-sky-400 border h-[8vh] w-[20vh]">A1</button>
+        </div>
+      </div>
+      <div className="flex gap-2">
+        <div className="flex-1 border border-sky-400 flex justify-center h-[40vh] rounded-3xl items-center bg-white bg-opacity-70">
+          <button className="border-sky-400 border h-[8vh] w-[20vh]">A2</button>
+        </div>
+        <div className="flex-1 border border-sky-400 flex justify-center rounded-3xl items-center bg-white bg-opacity-70">
+          <button className="border-sky-400 border h-[8vh] w-[20vh]">B1</button>
+        </div>
+      </div>
+    </div>
+  );
+};
